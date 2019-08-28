dd1752bec2b9f83e7950c54bbe79598f21d6edd6
Pannu <hpannu@iu.edu>
2019-08-28 17:12:55 -0400

    finished introduction

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+print('Hello, I am Herleen Pannu!') 
+print('My favorite game would have to be League Of Legends.')
+print('Some concerns I have for the class is that I might find things difficult as things continue. However at the moment, this class heavily reminds me of K201 so I am fairly confident now that I will be able to keep up well enough.')
+print('I am excited about the long weekend we have coming up, but it also feels weird to be missing a class so early in the semester.')
+print('My stackoverflow.com user number is 11991323') 
+print('My URL for my github.com profile is https://github.com/hpannu17')
